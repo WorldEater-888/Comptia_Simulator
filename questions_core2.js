9cf3b1bf98740b2051a1d2f4444b78864a766c70
World_Eater_Simulator <derek.n.delong@gmail.com>
2026-01-05 22:40:59 -0600

    Create questions_core2.js

---
+++ questions_core2.js
@@ -0,0 +1,16 @@
+/* ========================================================================== */
+/* CORE 2 QUESTIONS MODULE                                                  */
+/* ========================================================================== */
+
+// Check if the bucket exists
+if (window.MASTER_DATABASE) {
+    window.MASTER_DATABASE.push(
+
+
+);
+    
+    console.log(">> LOADED: CORE 2 QUESTIONS");
+    
+} else {
+    console.error("CRITICAL ERROR: MASTER_DATABASE NOT FOUND. CHECK DB_INIT.JS");
+}
